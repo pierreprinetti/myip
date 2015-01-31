fd35a42e1843b3107f52414ea1ab7393c65986e8
qrawl <pierre@prinetti.it>
2015-01-31 18:42:15 +0100

    cmon the server

--- serve.js
+++ serve.js
@@ -2,22 +2,24 @@ var dns = require('dns');
 var http = require('http');
 
 http.createServer(function(req, res) {
-  var callerIp = req.headers['x-forwarded-for']
-  console.log('Serving request from', callerIp)
+    var callerIp = req.headers['x-forwarded-for']
+    console.log('Serving request from', callerIp)
 
-  dns.reverse(callerIp, function(err, domains) {
-    if (err) {
-      res.writeHead(500, {
-        'Content-Type': 'text/plain'
-      });
-      res.end('Internal server error.');
-    } else {
-      res.writeHead(200, {
-        'Content-Type': 'text/plain'
-      });
-      res.write('Your IP:', callerIp);
-      res.write('Reverse DNS:', JSON.stringify(domains));
-      res.end();
-    }
-  });
-});
+    dns.reverse(callerIp, function(err, domains) {
+      if (err) {
+        res.writeHead(500, {
+          'Content-Type': 'text/plain'
+        });
+        res.end('Internal server error.');
+      } else {
+        res.writeHead(200, {
+          'Content-Type': 'text/plain'
+        });
+        res.write('Your IP:', callerIp);
+        res.write('Reverse DNS:', JSON.stringify(domains));
+        res.end();
+      }
+    });
+  })
+  .listen(8080, 'localhost');
+console.log('Server running at localhost:8080');
