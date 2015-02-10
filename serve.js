5579c6e8eabb9fd6bcb7f7d3c565956dcd588f5d
qrawl <pierre@prinetti.it>
2015-02-10 20:32:01 +0100

    overcomplicating it all

--- serve.js
+++ serve.js
@@ -1,40 +1,47 @@
+var express = require('express');
+var favicon = require('serve-favicon');
 var dns = require('dns');
 var http = require('http');
 
-http.createServer(function(req, res) {
-    var d = new Date();
-    var callerStr = req.headers['x-forwarded-for']
-    console.log(d.toJSON(), 'Serving request from', callerStr);
 
-    if (typeof callerStr === "undefined") {
-      res.writeHead(500, {
-        'Content-Type': 'text/plain; charset=utf-8"'
-      });
-      res.end("Unable to get caller IP.\n");
-    } else {
-      var callerIp = callerStr.split(', ')
-      res.writeHead(200, {
-        'Content-Type': 'text/plain; charset=utf-8"'
-      });
+var app = express();
+app.use(favicon(__dirname + '/public/favicon.ico'));
 
-      dns.reverse(callerIp[0], function(err, domains) {
-        if (err) {
-          res.write('Error while getting the reverse DNS for ' + callerIp[0] + '.\n');
-        } else {
-          res.write('Reverse DNS for ' + callerIp[0] + ': ' + domains + '\n');
-        }
-        if (callerIp.length > 1) {
-          dns.reverse(callerIp[1], function(err, domains) {
-            if (err) {
-              res.write('Error while getting the reverse DNS for ' + callerIp[1] + '.\n');
-            } else {
-              res.write('Reverse DNS for ' + callerIp[1] + ': ' + domains + '\n');
-            }
-          });
-        }
-        res.end();
-      });
-    }
-  })
-  .listen(8080, 'localhost');
+app.get('/', function(req, res) {
+  var d = new Date();
+  var callerStr = req.headers['x-forwarded-for']
+  console.log(d.toJSON(), 'Serving request from', callerStr);
+
+  if (typeof callerStr === "undefined") {
+    res.writeHead(500, {
+      'Content-Type': 'text/plain; charset=utf-8"'
+    });
+    res.end("Unable to get caller IP.\n");
+  } else {
+    var callerIp = callerStr.split(', ')
+    res.writeHead(200, {
+      'Content-Type': 'text/plain; charset=utf-8"'
+    });
+
+    dns.reverse(callerIp[0], function(err, domains) {
+      if (err) {
+        res.write('Error while getting the reverse DNS for ' + callerIp[0] + '.\n');
+      } else {
+        res.write('Reverse DNS for ' + callerIp[0] + ': ' + domains + '\n');
+      }
+      if (callerIp.length > 1) {
+        dns.reverse(callerIp[1], function(err, domains) {
+          if (err) {
+            res.write('Error while getting the reverse DNS for ' + callerIp[1] + '.\n');
+          } else {
+            res.write('Reverse DNS for ' + callerIp[1] + ': ' + domains + '\n');
+          }
+        });
+      }
+      res.end();
+    });
+  }
+});
+
+app.listen(8080, 'localhost');
 console.log('Server running at localhost:8080');
