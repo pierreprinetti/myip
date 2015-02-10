f7e73678e419269ef263c3f2f5398bc7ed303043
qrawl <pierre@prinetti.it>
2015-02-10 19:45:33 +0100

    handle double IP

--- serve.js
+++ serve.js
@@ -2,21 +2,33 @@ var dns = require('dns');
 var http = require('http');
 
 http.createServer(function(req, res) {
-    var callerIp = req.headers['x-forwarded-for']
-    console.log('Serving request from', callerIp)
+    var d = new Date();
+    var callerStr = req.headers['x-forwarded-for']
+    console.log(d.toJSON(), 'Serving request from', callerStr);
 
-    dns.reverse(callerIp, function(err, domains) {
-      res.writeHead(200, {
-        'Content-Type': 'text/plain'
+    if (typeof callerStr === "undefined") {
+      res.writeHead(500, {
+        'Content-Type': 'text/plain; charset=utf-8"'
       });
-      res.write('Your IP: ' + callerIp + '\n');
-      if (err) {
-        res.write('Error while getting the reverse DNS: ' + err + '\n');
-      } else {
-        res.write('Reverse DNS: ' + domains + '\n');
-      }
-      res.end();
-    });
+      res.end("Unable to get caller IP.\n");
+    } else {
+      var callerIp = callerStr.split(', ')
+      res.writeHead(200, {
+        'Content-Type': 'text/plain; charset=utf-8"'
+      });
+
+      callerIp.forEach(function(caller) {
+        console.log("Trying to reverse", caller)
+        dns.reverse(caller, function(err, domains) {
+          if (err) {
+            res.write('Error while getting the reverse DNS for ' + caller + '.\n');
+          } else {
+            res.write('Reverse DNS for ' + caller + ': ' + domains + '\n');
+          }
+        });
+      });
+      res.end("\n");
+    }
   })
   .listen(8080, 'localhost');
 console.log('Server running at localhost:8080');
